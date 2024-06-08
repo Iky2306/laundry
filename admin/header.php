11cd394faeb5247b3aae8a127239b0b6a69ad4b4
Iky2306 <ikymercedes@gmail.com>
2024-06-08 19:23:59 +0700

    update file

--- admin/header.php
+++ admin/header.php
@@ -1,8 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Sistem Informasi Laundry Malas Ngoding -
-WWW.MALASNGODING.COM</title>
+<title>Sistem Informasi Laundry</title>
 <link rel="stylesheet" type="text/css" 
 href="../assets/css/bootstrap.css">
 <script type="text/javascript" src="../assets/js/jquery.js"></script>
@@ -22,16 +21,15 @@ header("location:../index.php?pesan=belum_login");
 <div class="container-fluid">
 <div class="navbar-header">
 <button type="button" class="navbar-toggle collapsed" 
-data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" ariaexpanded="false">
+data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
 <span class="sr-only">Toggle navigation</span>
 <span class="icon-bar"></span>
 <span class="icon-bar"></span>
 <span class="icon-bar"></span>
 </button>
-<a class="navbar-brand" href="index.php">LAUNDRY 
-Malas Ngoding</a>
+<a class="navbar-brand" href="index.php">LAUNDRY</a>
 </div>
-<div class="collapse navbar-collapse" id="bs-examplenavbar-collapse-1">
+<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 <ul class="nav navbar-nav">
 <li class="active"><a href="index.php"><i 
 class="glyphicon glyphicon-home"></i> Dashboard</a></li>
@@ -58,6 +56,7 @@ glyphicon-log-out"></i> Log Out</a></li>
 <ul class="nav navbar-nav navbar-right">
 <li><p class="navbar-text">Halo, <b><?php echo 
 $_SESSION['username']; ?></b> !</p></li>
+<img src="" alt="">
 </ul>
 </div>
 </div>
--- index.php
+++ index.php
@@ -1,58 +1,63 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Sistem Informasi Laundry Malas Ngoding -
-www.malasngoding.com</title>
+<title>Sistem Informasi Laundry</title>
 <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
 <script type="text/javascript" src="assets/js/jquery.js"></script>
 <script type="text/javascript" src="assets/js/bootstrap.js"></script>
+<style>
+    body {
+        background-image: url("assets/img/bg-laundry.jpg");
+        background-size: cover;
+        background-position: center;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        margin: 0;
+    }
+    .panel {
+        background-color: rgba(255, 255, 255, 0.2); /* Semi-transparent white */
+    }
+    .panel {
+        box-shadow: 0 2px 5px rgba(0,0,0,0.1);
+        border-radius: 5px;
+    }
+    .form-control {
+        margin-bottom: 10px;
+    }
+</style>
 </head>
-<body style="background: #f0f0f0">
-<br/>
-<br/>
-<br/>
-<br/>
-<center>
-<h2>SISTEM INFORMASI LAUNDRY <br/> www.malasngoding.com</h2>
-</center>
-<br/>
-<br/>
-<br/>
-<br/>
+<body>
 <div class="container">
-<div class="col-md-4 col-md-offset-4">
-<?php 
-if(isset($_GET['pesan'])){
-if($_GET['pesan'] == "gagal"){
-echo "<div class='alert alert-danger'>Login 
-gagal! username dan password salah!</div>";
-}else if($_GET['pesan'] == "logout"){
-echo "<div class='alert alert-info'>Anda telah 
-berhasil logout</div>";
-}else if($_GET['pesan'] == "belum_login"){
-echo "<div class='alert alert-danger'>Anda 
-harus login untuk mengakses halaman admin</div>";
-}
-}
-?>
-<form action="login.php" method="post">
-<div class="panel">
-<br/>
-<div class="panel-body">
-<div class="form-group">
-<label>Username</label>
-<input type="text" class="form-control" name="username">
-</div>
-<div class="form-group">
-<label>Password</label>
-<input type="password" class="form-control" name="password">
-</div>
-<input type="submit" class="btn btn-primary" value="Log In">
-</div>
-<br/>
-</div>
-</form>
-</div>
+    <div class="col-md-4 col-md-offset-4">
+        <?php 
+        if(isset($_GET['pesan'])){
+            if($_GET['pesan'] == "gagal"){
+                echo "<div class='alert alert-danger'>Login gagal! username dan password salah!</div>";
+            } else if($_GET['pesan'] == "logout"){
+                echo "<div class='alert alert-info'>Anda telah berhasil logout</div>";
+            } else if($_GET['pesan'] == "belum_login"){
+                echo "<div class='alert alert-danger'>Anda harus login untuk mengakses halaman admin</div>";
+            }
+        }
+        ?>
+        <form action="login.php" method="post">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <div class="form-group">
+                        <label>Username</label>
+                        <input type="text" class="form-control" name="username">
+                    </div>
+                    <div class="form-group">
+                        <label>Password</label>
+                        <input type="password" class="form-control" name="password">
+                    </div>
+                    <input type="submit" class="btn btn-primary btn-block" value="Log In">
+                </div>
+            </div>
+        </form>
+    </div>
 </div>
 </body>
 </html>
